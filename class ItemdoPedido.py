99ccf5fa283f64b457fc37bc20c86a7bbcb85969
GabrielTorbes <gabrieltorbes@gmail.com>
2023-09-30 19:44:08 -0300

    commit class cliente.py

---
+++ class ItemdoPedido.py
@@ -0,0 +1,8 @@
+class Item_do_pedido:
+
+    def _init_(self, quantidade, preco_unitario, Pedido, produto):
+
+       self.quantidade= quantidade
+       self.preco_unitario= preco_unitario
+       self.Pedido= Pedido
+       self.produto= produto
---
+++ class cliente.py
@@ -0,0 +1,10 @@
+class Cliente:
+
+    def _init_(self,primeiro_nome, ultimo_nome, endereco, celular, email, genero):
+
+       self.primeiro_nome= primeiro_nome
+       self.ultimo_nome= ultimo_nome
+       self.endereco= endereco
+       self.celular= celular
+       self.email= email
+       self.genero= genero
