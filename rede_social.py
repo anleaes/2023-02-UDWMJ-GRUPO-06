8952510e493a1325909a053a56d6a1597b0df413
LuizEduardo-MS <luezardo@gmail.com>
2023-10-05 11:03:47 -0300

    agora sim

---
+++ rede_social.py
@@ -0,0 +1,6 @@
+class RedeSocial:
+
+    def __init__(self,nome,descricao):
+       self.nome = nome  
+       self.descricao = descricao
+
---
+++ rede_social_cliente.py
@@ -0,0 +1,5 @@
+class RedeSocialCliente:
+
+    def __init__(self, cliente, rede_social):
+        self.cliente = cliente
+        self.rede_social = rede_social
