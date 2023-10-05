87607986658319d8346c0537c0f12aa8be5f89e1
LuizEduardo-MS <luezardo@gmail.com>
2023-10-05 10:43:20 -0300

    arrumadas todas cagadas

--- class ItemdoPedido.py
+++
@@ -1,8 +0,0 @@
-class Item_do_pedido:
-
-    def __init__(self, quantidade, preco_unitario, Pedido, produto):
-
-       self.quantidade= quantidade
-       self.preco_unitario= preco_unitario
-       self.Pedido= Pedido
-       self.produto= produto
