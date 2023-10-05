852f56d3e58b7abcd6e4d3236d3db8223ae32c22
GabrielTorbes <gabrieltorbes@gmail.com>
2023-10-05 17:25:54 -0300

    Merge pull request #4 from anleaes/criar-metodo-do-pedido Método do Pedido

--- item_pedido.py
+++ item_pedido.py
@@ -5,3 +5,6 @@ class ItemPedido:
        self.preco_unitario = preco_unitario
        self.Pedido = Pedido
        self.produto = produto
+
+    def mostra_qual_o_produto_e_o_preço(self):
+        print("O item selecionado foi %s ele custará %i")
