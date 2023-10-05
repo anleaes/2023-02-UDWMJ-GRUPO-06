ecf0920e0c8f3b59613dea36b8d3f1588a62d9d3
LuizEduardo-MS <luezardo@gmail.com>
2023-10-05 11:06:05 -0300

    criado o metodo categoria

--- categoria.py
+++ categoria.py
@@ -3,3 +3,6 @@ class Categoria:
     def __init__(self, nome, descricao):
         self.nome = nome
         self.descricao = descricao
+
+    def imprime_informacoes_categorias(self):
+        print("A categoria é %s e a descricao é %s.")
