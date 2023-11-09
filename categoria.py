83cdc0e4eaafa66112d4af1c81e5b01b54771d11
LuizEduardo-MS <146347155+LuizEduardo-MS@users.noreply.github.com>
2023-11-09 11:25:58 -0300

    Delete categoria.py

--- categoria.py
+++
@@ -1,8 +0,0 @@
-class Categoria:
-
-    def __init__(self, nome, descricao):
-        self.nome = nome
-        self.descricao = descricao
-
-    def imprime_informacoes_categorias(self):
-        print("A categoria do produto é %s e a descricao é %s.{self.nome}: {self.descricao}")
