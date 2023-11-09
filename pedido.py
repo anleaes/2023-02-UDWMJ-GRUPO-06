b91516fe89648dcc70e2adafc1f22591136b6849
LuizEduardo-MS <146347155+LuizEduardo-MS@users.noreply.github.com>
2023-11-09 11:25:29 -0300

    Delete pedido.py

--- pedido.py
+++
@@ -1,9 +0,0 @@
-class Pedido:
-    
-    def __init__(self, preco_total, status):
-        self.preco_total = preco_total
-        self.status = status
-
-    def mostra_preço_do_pedido(self):
-        print("O seu pedido vai custar R$ %i:{self.preco_total}")
-        
