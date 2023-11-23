218a136b6c9b44ef7d866c6afc760721bac7ee73
LuizEduardo-MS <luezardo@gmail.com>
2023-11-23 14:04:33 -0300

    settings e urls ajustadas

--- AcademiaAPP/settings.py
+++ AcademiaAPP/settings.py
@@ -42,6 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django-widget-tweaks',
+    'core.apps.CoreConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
